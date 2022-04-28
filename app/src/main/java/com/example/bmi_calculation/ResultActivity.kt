894749a0e0c38d5b91c9dfe317ca17bf933f937c
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-29 02:24:34 +0900

    intent를 가져와서 result에서 BMI수치 계산 후 보여주기

--- app/src/main/java/com/example/bmi_calculation/ResultActivity.kt
+++ app/src/main/java/com/example/bmi_calculation/ResultActivity.kt
@@ -2,10 +2,35 @@ package com.example.bmi_calculation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlin.math.pow
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val height = intent.getIntExtra("height", 0)
+        val weight = intent.getIntExtra("weight", 0)
+
+        Log.d("ResultActivity", "height : $height, weight : $weight")
+
+        val bmi = weight / (height / 100.0).pow(2.0) // 코틀린은 double 타입이 pow 함수를 갖고 있다
+        val resultText = when {
+            bmi >= 35.0 -> "고도 비만"
+            bmi >= 30.0 -> "중정도 비만"
+            bmi >= 25.0 -> "경도 비만"
+            bmi >= 23.0 -> "과체중"
+            bmi >= 18.5 -> "정상체중"
+            else -> "저체중"
+        }
+
+        val resultValueTextView = findViewById<TextView>(R.id.tv_result_title_value)
+        val resultStringTextView = findViewById<TextView>(R.id.tv_result_content_value)
+
+        resultValueTextView.text = bmi.toString()
+        resultStringTextView.text = resultText
+
     }
 }
