f955ec6eb1169d91557dcbb79ca49c2d18767022
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-28 23:17:55 +0900

    BMI계산기 프로젝트 생성

---
+++ app/src/main/java/com/example/bmi_calculation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bmi_calculation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
